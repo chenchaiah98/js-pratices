1a368fc9d5ff868aa7a8f85f6d075b48ba444b04
chenchaiah98 <rchenchaiah98@gmail.com>
2023-07-08 11:42:00 +0530

    test files

---
+++ typeScript/Interfaces & APIs.ts
@@ -0,0 +1,15 @@
+// Start with unit test for below task
+
+// Use interface to define an User with below keys:
+//     id: number
+//     name: string
+//     username: string
+//     email: string
+
+// Then use below API to get user details:
+//     https://jsonplaceholder.typicode.com/users/<userId>
+
+// Display the above user information in a logger:
+//     Use a logger npm like morgan or winston
+
+
