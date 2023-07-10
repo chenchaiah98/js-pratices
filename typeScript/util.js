05583a53f2b2b601100b9700fe2b29acddd6e078
chenchaiah98 <rchenchaiah98@gmail.com>
2023-07-10 20:43:34 +0530

    testcase changes

--- typeScript/util.js
+++ typeScript/util.js
@@ -1,29 +1,19 @@
-// function sum (a, b){
-//     return a + b;
-// }
-
-// module.exports = { sum }
-
-
-
-// interface data {
-//     id:number;
-//     name: string;
-//     username:string;
-//     email: number;
-// }
-
 let url = `https://jsonplaceholder.typicode.com/users/1`
 
 
-
- async function fetchFunction(url1){
-   let responce =await fetch(url1)
-    let data = await responce;
-        // return userData.id;
-        console.log(data);
-        return(data.id);
+async function fetchFunction(url){
+    try
+    {
+        let responce = await fetch(url);
+        let data = await responce.json();
+        let id = await data.id;
+        return id;
+    }
+    catch(error){
+        console.log(error);
+    }
 }
-// fetchFunction(url)
 
-exports = { fetchFunction };
+
+
+module.exports = { fetchFunction }
