abdbd149b640b239b951ad824890b2051fca1a29
chenchaiah98 <rchenchaiah98@gmail.com>
2023-07-10 20:43:48 +0530

    test

--- typeScript/__test__/test.js
+++ typeScript/__test__/test.js
@@ -1,42 +1,12 @@
+const {fetchFunction} = require('../util');
 
-// const { sum } = require('../util');
-
-const {fetchFunction} =require('../util');
-test('data of id : 1',()=>{
-  const result = fetchFunction(`https://jsonplaceholder.typicode.com/users/1`);
-  expect(result).toBe(1);
+test('data of id : 2',async ()=>{
+  const result = await fetchFunction(`https://jsonplaceholder.typicode.com/users/2`);
+  expect(result).toBe(2);
 });
 
 
-// // All below tests should be based on 'Acceptance Criteria'
-// test('adds two positive numbers correctly', () => {
-//   const result = sum(2, 3);
-//   expect(result).toBe(5);
-// });
-
-// test('adds a positive number and zero correctly', () => {
-//   const result = sum(4, 0);
-//   expect(result).toBe(4);
-// });
-
-// test('adds a positive number and a negative number correctly', () => {
-//   const result = sum(6, -3);
-//   expect(result).toBe(3);
-// });
-
-// test('adds two negative numbers correctly', () => {
-//   const result = sum(-8, -2);
-//   expect(result).toBe(-10);
-// });
-
-// // Bad test case.
-// test('adds two strings', () => {
-//     const result = sum('10', '10')
-//     expect(result).toBe('1010')
-// })
-
-// /*
-// test('', function(){
-  
-// })
-// */
+test('data of id : 1', async () => {
+  const result = await fetchFunction(`https://jsonplaceholder.typicode.com/users/1`);
+  expect(result).toBe(1);
+});
